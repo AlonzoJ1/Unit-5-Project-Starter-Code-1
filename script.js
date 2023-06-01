832e44fda2da8bf5a48090b3227480c2566e8e0f
AlonzoJ1 <135248264+AlonzoJ1@users.noreply.github.com>
2023-06-01 15:43:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let sumbitbutton = document.querySelector(".submit-button");
+let message = document.querySelector(".message");
+let input=document.querySelector(".comment");
+let words1=document.querySelector(".words1");
+let words= document.querySelector(".words");
+sumbitbutton.onclick=function(){
+  let input1=words.value
+  let input2=words.value
+if (input >=1 && input1 <=5)& input==="Dogs"){
+  result.innerHTML="because you picked " + input1 +"and" + input2 + "you are a morning person"
+}
+
+else if (input >=1 && input1 <=5)& input2==="Cats"){
+ result.innerHTML="because you picked " + input1 +"and" + input2 + "you are a morning person"
+
+  
+  else if (input >=6 && input1 <=10)& input2==="Hot coffe")
+{
+ result.innerHTML="because you picked " + input1 +"and" + input2 + "you are a morning person"
+  
+   else if (input >=6 && input1 <=10)& input2==="cold coffe"){
+      result.innerHTML="because you picked " + input1 +"and" + input2 + "you are a morning person"
+   }
+  else{
+    
+  }
+};
